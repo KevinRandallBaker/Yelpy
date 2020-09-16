77be74709a87042ca4c3c5d58ea4b47e85099309
kbaker42 <kevinrandallbaker@gmail.com>
2020-09-16 10:34:54 -0700

    Initial Commit

---
+++ Yelpy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yelpy
+//
+//  Created by Laura Baker on 9/16/20.
+//  Copyright © 2020 Kevin Baker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
